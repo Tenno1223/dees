df88f0438927ab2bdff09271a1fcf6c8fbd6d4ca
Tenno1223 <xdeescount@gmail.com>
2023-09-21 13:56:49 +0300

    Added dees files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # dees
 my name is mario
+https://github.com/Tenno1223/dees repository link
--- main.cpp
+++ main.cpp
@@ -2,3 +2,4 @@
 def get_vowels(String):
     return [each for each in String if each in "aeiou"]
 get_vowels("animal") # [a, i, a]
+https://github.com/Tenno1223/dees repository link 
